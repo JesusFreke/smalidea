6c2ee7ce4303dd3ad8ccc708450e2d63036b00cb
Ben Gruver <jesusfreke@jesusfreke.com>
2021-02-18 15:57:23 -0800

    Update ErrorReportor class to not use deleted ErrorBean class

--- src/main/java/org/jf/smalidea/errorReporting/ErrorReporter.java
+++ src/main/java/org/jf/smalidea/errorReporting/ErrorReporter.java
@@ -17,9 +17,7 @@
 package org.jf.smalidea.errorReporting;
 
 import com.intellij.diagnostic.IdeErrorsDialog;
-import com.intellij.diagnostic.LogMessageEx;
 import com.intellij.diagnostic.ReportMessages;
-import com.intellij.errorreport.bean.ErrorBean;
 import com.intellij.ide.DataManager;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
@@ -28,6 +26,10 @@ import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ApplicationNamesInfo;
+import com.intellij.openapi.application.ex.ApplicationInfoEx;
 import com.intellij.openapi.diagnostic.ErrorReportSubmitter;
 import com.intellij.openapi.diagnostic.IdeaLoggingEvent;
 import com.intellij.openapi.diagnostic.SubmittedReportInfo;
@@ -35,9 +37,16 @@ import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.progress.EmptyProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.updateSettings.impl.UpdateSettings;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.Consumer;
+import com.intellij.util.SystemProperties;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
+import java.util.Calendar;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -53,34 +62,18 @@ public class ErrorReporter extends ErrorReportSubmitter {
     return "Report as issue on smali's github repo";
   }
 
+
   @Override
-  public boolean submit(IdeaLoggingEvent[] events, String additionalInfo, Component parentComponent,
-                        final Consumer<SubmittedReportInfo> consumer) {
+  public boolean submit(IdeaLoggingEvent[] events, @Nullable String additionalInfo, @NotNull Component parentComponent, @NotNull Consumer<? super SubmittedReportInfo> consumer) {
     IdeaLoggingEvent event = events[0];
-    ErrorBean bean = new ErrorBean(event.getThrowable(), IdeaLogger.ourLastActionId);
 
     final DataContext dataContext = DataManager.getInstance().getDataContext(parentComponent);
-
-    bean.setDescription(additionalInfo);
-    bean.setMessage(event.getMessage());
-
     IdeaPluginDescriptor plugin = IdeErrorsDialog.getPlugin(event);
     if (plugin == null) {
       plugin = PluginManager.getPlugin(PluginId.getId("org.jf.smalidea"));
     }
 
-    if (plugin != null) {
-      bean.setPluginName(plugin.getName());
-      bean.setPluginVersion(plugin.getVersion());
-    }
-
-    Object data = event.getData();
-
-    if (data instanceof LogMessageEx) {
-      bean.setAttachments(((LogMessageEx)data).getAttachments());
-    }
-
-    Map<String, String> reportValues = ITNProxy.createParameters(bean);
+    Map<String, String> reportValues = createParameters(plugin, additionalInfo, event.getMessage());
 
     final Project project = CommonDataKeys.PROJECT.getData(dataContext);
 
@@ -112,7 +105,11 @@ public class ErrorReporter extends ErrorReportSubmitter {
       }
     };
 
-    GithubFeedbackTask task = new GithubFeedbackTask(project, "Submitting error report", true,  reportValues,
+    GithubFeedbackTask task = new GithubFeedbackTask(
+            project,
+            "Submitting error report",
+            true,
+            reportValues,
             successCallback, errorCallback);
 
     if (project == null) {
@@ -121,5 +118,53 @@ public class ErrorReporter extends ErrorReportSubmitter {
       ProgressManager.getInstance().run(task);
     }
     return true;
+  }
+
+  public static Map<String, String> createParameters(
+          @Nullable IdeaPluginDescriptor plugin,
+          String errorDescription,
+          String errorMessage) {
+
+    Map<String, String> params = new LinkedHashMap<>();
+
+    params.put("protocol.version", "1");
+
+    params.put("os.name", SystemProperties.getOsName());
+    params.put("java.version", SystemProperties.getJavaVersion());
+    params.put("java.vm.vendor", SystemProperties.getJavaVmVendor());
+
+    ApplicationInfoEx appInfo = ApplicationInfoEx.getInstanceEx();
+    ApplicationNamesInfo namesInfo = ApplicationNamesInfo.getInstance();
+    Application application = ApplicationManager.getApplication();
+    params.put("app.name", namesInfo.getProductName());
+    params.put("app.name.full", namesInfo.getFullProductName());
+    params.put("app.name.version", appInfo.getVersionName());
+    params.put("app.eap", Boolean.toString(appInfo.isEAP()));
+    params.put("app.internal", Boolean.toString(application.isInternal()));
+    params.put("app.build", appInfo.getBuild().asString());
+    params.put("app.version.major", appInfo.getMajorVersion());
+    params.put("app.version.minor", appInfo.getMinorVersion());
+    params.put("app.build.date", formatDate(appInfo.getBuildDate()));
+    params.put("app.build.date.release", formatDate(appInfo.getMajorReleaseBuildDate()));
+
+    UpdateSettings updateSettings = UpdateSettings.getInstance();
+    params.put("update.channel.status", updateSettings.getSelectedChannelStatus().getCode());
+    params.put("update.ignored.builds", StringUtil.join(updateSettings.getIgnoredBuildNumbers(), ","));
+
+    if (plugin != null) {
+      params.put("plugin.name", plugin.getName());
+      params.put("plugin.version", plugin.getVersion());
+    }
+
+    params.put("last.action", IdeaLogger.ourLastActionId);
+
+    params.put("error.message", errorMessage);
+    params.put("error.description", errorDescription);
+
+    return params;
+  }
+
+  private static String formatDate(Calendar calendar) {
+    return calendar == null ?  null : Long.toString(calendar.getTime().getTime());
   }
 }
